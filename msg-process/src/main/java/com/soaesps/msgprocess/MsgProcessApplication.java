2f7709620efe192a593ed0cb49a85e896d306602
Александр Викторов <alexandervas1985@outlook.com>
2019-01-02 14:20:48 +0300

    #Adding msgprocess application

---
+++ msg-process/src/main/java/com/soaesps/msgprocess/MsgProcessApplication.java
@@ -0,0 +1,73 @@
+package com.soaesps.msgprocess;
+
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+@SpringBootApplication
+@EnableResourceServer
+@EnableDiscoveryClient
+@EnableOAuth2Client
+@EnableFeignClients
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableConfigurationProperties
+@Configuration
+public class MsgProcessApplication extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private ResourceServerTokenServices tokenServices;
+
+    @Autowired
+    private ResourceServerProperties rsp;
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(MsgProcessApplication.class, args);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "security.oauth2.client")
+    public ClientCredentialsResourceDetails clientCredentialsResourceDetails() {
+        return new ClientCredentialsResourceDetails();
+    }
+
+    @Bean
+    public RequestInterceptor oauth2FeignRequestInterceptor(){
+        return new OAuth2FeignRequestInterceptor(new DefaultOAuth2ClientContext(), clientCredentialsResourceDetails());
+    }
+
+    @Bean
+    public OAuth2RestTemplate clientCredentialsRestTemplate() {
+        return new OAuth2RestTemplate(clientCredentialsResourceDetails());
+    }
+
+    @Bean
+    public ResourceServerTokenServices tokenServices() {
+        return tokenServices;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.anonymous().disable()
+                .authorizeRequests()
+                .antMatchers("/applications/**").permitAll().anyRequest().authenticated()
+                .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+    }
+}
