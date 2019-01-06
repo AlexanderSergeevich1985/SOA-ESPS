d9fdeeb7bcbfe9a108fe7ebacddfc6b9164cc9f6
Александр Викторов <alexandervas1985@outlook.com>
2019-01-06 17:58:25 +0300

    #Adding user info

---
+++ common-functional/src/main/java/com/soaesps/core/DataModels/security/UserInfo.java
@@ -0,0 +1,89 @@
+package com.soaesps.core.DataModels.security;
+
+import com.soaesps.core.DataModels.BaseEntity;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+
+@Entity
+@Table(name = "USER_INFO")
+public class UserInfo extends BaseEntity {
+    @Column(name = "first_name", length = 40)
+    private String firstName;
+
+    @Column(name = "middle_name", length = 50)
+    private String middleName;
+
+    @Column(name = "last_name", length = 70)
+    private String lastName;
+
+    @Column(name = "BIRTHDAY", length = 70)
+    private String birthday;
+
+    @Column(name = "EMAIL", unique = true, nullable = false)
+    @Email
+    private String email;
+
+    @Column(name = "TELEPHONE", length = 35)
+    private String telephone;
+
+    public UserInfo() {}
+
+    @Nullable
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@Nullable String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Nullable
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(@Nullable String middleName) {
+        this.middleName = middleName;
+    }
+
+    @Nullable
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@Nullable String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Nullable
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(@Nullable String birthday) {
+        this.birthday = birthday;
+    }
+
+    @Nonnull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Nonnull String email) {
+        this.email = email;
+    }
+
+    @Nullable
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(@Nullable String telephone) {
+        this.telephone = telephone;
+    }
+}
