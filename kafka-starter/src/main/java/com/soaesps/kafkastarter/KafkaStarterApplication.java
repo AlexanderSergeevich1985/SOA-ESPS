2e02940e751811c73970e6167414d30f1bf6393c
Александр Викторов <alexandervas1985@outlook.com>
2019-01-15 13:14:17 +0300

    #Adding kafka starter application

---
+++ kafka-starter/src/main/java/com/soaesps/kafkastarter/KafkaStarterApplication.java
@@ -0,0 +1,11 @@
+package com.soaesps.kafkastarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStarterApplication.class, args);
+    }
+}
