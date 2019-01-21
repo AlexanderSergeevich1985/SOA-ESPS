4e5f57d1a24ead154b44092063bdde05e06fb01b
Александр Викторов <alexandervas1985@outlook.com>
2019-01-21 14:51:39 +0300

    #Adding default exception hadler

---
+++ documents-service/src/main/java/com/soaesps/documentsservice/controller/DefaultExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.soaesps.documentsservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@ControllerAdvice
+public class DefaultExceptionHandler {
+    public static Logger logger;
+
+    static {
+        logger = Logger.getLogger(DefaultExceptionHandler.class.getName());
+        logger.setLevel(Level.INFO);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public @ResponseBody
+    ResponseEntity<?> handleException(final Exception ex) {
+        if(logger.isLoggable(Level.INFO)) {
+            logger.log(Level.INFO, "Exception occurred: ", ex);
+        }
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
