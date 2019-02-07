54d17c13bfe21516ede5384dfbd99a5857309883
Александр Викторов <alexandervas1985@outlook.com>
2019-02-08 01:48:26 +0300

    #Adding profile test

--- profile-service/src/test/java/com/soaesps/profile/controller/ProfileControllerTest.java
+++ profile-service/src/test/java/com/soaesps/profile/controller/ProfileControllerTest.java
@@ -1,8 +1,10 @@
 package com.soaesps.profile.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.soaesps.core.DataModels.security.UserProfile;
 import com.soaesps.profile.ProfileApplication;
 import com.soaesps.profile.service.ProfileService;
+import com.sun.security.auth.UserPrincipal;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,7 +17,11 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = ProfileApplication.class)
@@ -41,5 +47,18 @@ public class ProfileControllerTest {
     public void contexLoads() throws Exception {
         Assert.assertNotNull(controller);
         Assert.assertNotNull(service);
+    }
+
+    @Test
+    public void testGetCurrentUserProfile() throws Exception {
+        final UserProfile profile = new UserProfile();
+        profile.setUserName("test");
+
+        when(service.getUserProfile(profile.getUserName())).thenReturn(profile);
+
+        mockMvc.perform(get("/profiles/current")
+                .principal(new UserPrincipal(profile.getUserName())))
+                .andExpect(jsonPath("$.userName").value(profile.getUserName()))
+                .andExpect(status().isOk());
     }
 }
