299ccbef3375b6c8e668949e95eed9b78ad25fa9
Александр Викторов <alexandervas1985@outlook.com>
2019-03-03 13:03:01 +0300

    #Adding profile test

---
+++ aggregator/src/main/java/com/soaesps/aggregator/config/RabbitMQConfig.java
@@ -0,0 +1,81 @@
+package com.soaesps.aggregator.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitMQConfig implements RabbitListenerConfigurer {
+    @Value("${rabbitmq.queueName}")
+    private String queueName;
+
+    @Value("${rabbitmq.topicExchangeName}")
+    private String topicExchangeName;
+
+    @Value("${rabbitmq.host}")
+    private String host;
+
+    @Value("${rabbitmq.username}")
+    private String username;
+
+    @Value("${rabbitmq.password}")
+    private String password;
+
+    @Value("${rabbitmq.timeout}")
+    private long timeout;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    public CachingConnectionFactory cachingConnectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(cachingConnectionFactory());
+        template.setReplyTimeout(timeout);
+        return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory configureListenerContainerFactory() {
+        final SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setDefaultRequeueRejected(false);
+        factory.setConnectionFactory(cachingConnectionFactory());
+        return factory;
+    }
+
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar rabbitListenerEndpointRegistrar) {
+        rabbitListenerEndpointRegistrar.setContainerFactory(configureListenerContainerFactory());
+    }
+}
