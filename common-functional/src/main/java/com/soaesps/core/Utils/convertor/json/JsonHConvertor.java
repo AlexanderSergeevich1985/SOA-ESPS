b373318e8c8f7978c82ed3bdc89def813b698743
Александр Викторов <alexandervas1985@outlook.com>
2019-06-30 20:01:15 +0300

    #Adding json hibernate convertor

---
+++ common-functional/src/main/java/com/soaesps/core/Utils/convertor/json/JsonHConvertor.java
@@ -0,0 +1,33 @@
+package com.soaesps.core.Utils.convertor.json;
+
+import com.soaesps.core.Utils.JsonUtil;
+
+import javax.annotation.Nullable;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.lang.reflect.ParameterizedType;
+
+@Converter
+public class JsonHConvertor<T> implements AttributeConverter<T, String> {
+    private Class<T> clazz;
+
+    @Nullable
+    @Override
+    public String convertToDatabaseColumn(final T entity) {
+        return JsonUtil.toString(entity);
+    }
+
+    @Override
+    public T convertToEntityAttribute(final String data) {
+        return JsonUtil.fromString(data, getType());
+    }
+
+    public Class<T> getType() {
+        if (clazz == null) {
+            ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
+            clazz = (Class<T>) type.getActualTypeArguments()[0];
+        }
+
+        return clazz;
+    }
+}
