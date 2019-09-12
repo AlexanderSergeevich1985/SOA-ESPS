6047f07a28a9bbfe3bc18b75d9cd2816ed97a7f4
Александр Викторов <alexandervas1985@outlook.com>
2019-09-13 02:55:54 +0300

    #Add security configuration

--- common-functional/src/main/java/com/soaesps/core/component/balancer/BaseLoadBalancer.java
+++ common-functional/src/main/java/com/soaesps/core/component/balancer/BaseLoadBalancer.java
@@ -1,17 +1,19 @@
 package com.soaesps.core.component.balancer;
 
-import com.soaesps.core.BaseOperation.Statistics.LightMeanCalculator;
+import com.soaesps.core.BaseOperation.Statistics.LightDeviationCalculator;
 import com.soaesps.core.DataModels.executor.ExecutorNode;
-import com.soaesps.core.DataModels.executor.NodeStatistic;
 import com.soaesps.core.DataModels.task.BaseJobDesc;
 import com.soaesps.core.Utils.DateTimeHelper;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.TimeUnit;
 
 public class BaseLoadBalancer {
     private ConcurrentHashMap<String, UsedNodeWrapper> usedNodesReg = new ConcurrentHashMap<>();
@@ -20,7 +22,25 @@ public class BaseLoadBalancer {
 
     private ConcurrentSkipListSet<NodeWrapper> nodes = new ConcurrentSkipListSet<>(createComparator());
 
+    private ConcurrentHashMap<String, JobDesc> refJobsDescs = new ConcurrentHashMap<>();
+
     public BaseLoadBalancer() {
+    }
+
+    public ConcurrentHashMap<String, JobDesc> getRefJobsDescs() {
+        return refJobsDescs;
+    }
+
+    public void setRefJobsDescs(final ConcurrentHashMap<String, JobDesc> refJobsDescs) {
+        this.refJobsDescs = refJobsDescs;
+    }
+
+    public void addRefJobDesc(final String jobKey, final JobDesc jobDesc) {
+        refJobsDescs.put(jobKey, jobDesc);
+    }
+
+    public JobDesc getRefJobDesc(final String jobKey) {
+        return refJobsDescs.get(jobKey);
     }
 
     public NodeWrapper getBestNodeToUse() {
@@ -30,10 +50,14 @@ public class BaseLoadBalancer {
         return nodeWrapper;
     }
 
-    public NodeWrapper updateNodeStatistic(final String nodeKey, final String jobKey) {
+    public NodeWrapper updateNodeStatistic(final String nodeKey, final String jobKey, final Long newValue) {
         UsedNodeWrapper nodeWrapper = usedNodesReg.get(nodeKey);
-        JobDescWrapper jobDescWrapper = nodeWrapper.get
-        usedNodes.add(new UsedNodeWrapper(nodeWrapper));
+        JobDesc refJobDesc = refJobsDescs.get(jobKey);
+        if (refJobDesc == null || refJobDesc.getCalculator() == null) {
+            return null;
+        }
+        double refMean = refJobDesc.getCalculator().getMean();
+        nodeWrapper.getLightDeviationCalculator().update(newValue/refMean);
 
         return nodeWrapper;
     }
@@ -69,34 +93,33 @@ public class BaseLoadBalancer {
     }
 
     static public class UsedNodeWrapper extends NodeWrapper {
-        private ConcurrentHashMap<String, JobDescWrapper> jobs = new ConcurrentHashMap<>();
+        private ConcurrentHashMap<String, UsedJobDesc> jobs = new ConcurrentHashMap<>();
 
         public UsedNodeWrapper(final NodeWrapper nodeWrapper) {
-            super(nodeWrapper.getExecutorNode(), nodeWrapper.getLightMeanCalculator());
+            super(nodeWrapper.getExecutorNode(), nodeWrapper.getLightDeviationCalculator());
         }
 
-        public UsedNodeWrapper(final ExecutorNode executorNode, final LightMeanCalculator lightMeanCalculator) {
-            super(executorNode, lightMeanCalculator);
+        public UsedNodeWrapper(final ExecutorNode executorNode, final LightDeviationCalculator lightDeviationCalculator) {
+            super(executorNode, lightDeviationCalculator);
         }
 
-        public JobDescWrapper getJobsDesc() {
-            return jobs.;
+        public UsedJobDesc getJobsDesc(final String jobKey) {
+            return jobs.get(jobKey);
         }
 
-        public void setJobDesc(final JobDescWrapper jobDescWrapper) {
-            jobs.put(jobDescWrapper.getJobKey(), jobDescWrapper);
-            jobDescWrapper.startJobCount();
+        public void setJobDesc(final UsedJobDesc usedJobDesc) {
+            jobs.put(usedJobDesc.getJobKey(), usedJobDesc);
         }
     }
 
     static public class NodeWrapper {
         private ExecutorNode executorNode;
 
-        private LightMeanCalculator lightMeanCalculator;
+        private LightDeviationCalculator lightDeviationCalculator;
 
-        public NodeWrapper(final ExecutorNode executorNode, final LightMeanCalculator lightMeanCalculator) {
+        public NodeWrapper(final ExecutorNode executorNode, final LightDeviationCalculator lightDeviationCalculator) {
             this.executorNode = executorNode;
-            this.lightMeanCalculator = lightMeanCalculator;
+            this.lightDeviationCalculator = lightDeviationCalculator;
         }
 
         public ExecutorNode getExecutorNode() {
@@ -107,24 +130,62 @@ public class BaseLoadBalancer {
             this.executorNode = executorNode;
         }
 
-        public LightMeanCalculator getLightMeanCalculator() {
-            return lightMeanCalculator;
+        public LightDeviationCalculator getLightDeviationCalculator() {
+            return lightDeviationCalculator;
         }
 
-        public void setLightMeanCalculator(final LightMeanCalculator lightMeanCalculator) {
-            this.lightMeanCalculator = lightMeanCalculator;
+        public void setLightDeviationCalculator(LightDeviationCalculator lightDeviationCalculator) {
+            this.lightDeviationCalculator = lightDeviationCalculator;
         }
     }
 
-    static class JobDescWrapper {
-        private String jobKey;
+    static public class UsedJobDesc extends JobDesc {
+        private Map<String, DateTimeHelper.StopWatch> stopWatchs = new HashMap<>();
 
-        private DateTimeHelper.StopWatch stopWatch = new DateTimeHelper.StopWatch();
+        public UsedJobDesc(final BaseJobDesc jobDesc, final LightDeviationCalculator calculator) {
+            super(jobDesc, calculator);
+        }
+
+        public void startJobCount(final String jobId) {
+            final LocalDateTime dateTime = LocalDateTime.now();
+            DateTimeHelper.StopWatch stopWatch = stopWatchs.get(jobId);
+            stopWatch.setStart(dateTime.toInstant(ZoneOffset.UTC));
+        }
+
+        public void stopJobCount(final String jobId) {
+            final LocalDateTime dateTime = LocalDateTime.now();
+            DateTimeHelper.StopWatch stopWatch = stopWatchs.get(jobId);
+            stopWatch.setStart(dateTime.toInstant(ZoneOffset.UTC));
+            updateStats(stopWatch.getTimeMeasurement(TimeUnit.MILLISECONDS).doubleValue());
+        }
+
+        public DateTimeHelper.StopWatch getStopWatch(final String jobId) {
+            return stopWatchs.get(jobId);
+        }
+
+        public void setStopWatch(final String jobId, final DateTimeHelper.StopWatch stopWatch) {
+            this.stopWatchs.put(jobId, stopWatch);
+        }
+    }
+
+    static public class JobDesc {
+        private String jobKey;
 
         private BaseJobDesc jobDesc;
 
-        public JobDescWrapper(final BaseJobDesc jobDesc) {
+        private LightDeviationCalculator calculator;
+
+        public JobDesc(final BaseJobDesc jobDesc, final LightDeviationCalculator calculator) {
             this.jobDesc = jobDesc;
+            this.calculator = calculator;
+        }
+
+        public double updateStats(final Double newValue) {
+            return calculator.update(newValue);
+        }
+
+        public double updateStats(final Double oldValue, final Double newValue) {
+            return calculator.update(oldValue, newValue);
         }
 
         public String getJobKey() {
@@ -135,18 +196,6 @@ public class BaseLoadBalancer {
             this.jobKey = jobKey;
         }
 
-        public void startJobCount() {
-            final LocalDateTime dateTime = LocalDateTime.now();
-            this.stopWatch.setStart(dateTime.toInstant(ZoneOffset.UTC));
-            jobDesc.setStartTime(dateTime);
-        }
-
-        public void stopJobCount() {
-            final LocalDateTime dateTime = LocalDateTime.now();
-            this.stopWatch.setStop(dateTime.toInstant(ZoneOffset.UTC));
-            jobDesc.setEndTime(dateTime);
-        }
-
         public BaseJobDesc getJobDesc() {
             return jobDesc;
         }
@@ -155,12 +204,12 @@ public class BaseLoadBalancer {
             this.jobDesc = jobDesc;
         }
 
-        public DateTimeHelper.StopWatch getStopWatch() {
-            return stopWatch;
+        public LightDeviationCalculator getCalculator() {
+            return calculator;
         }
 
-        public void setStopWatch(final DateTimeHelper.StopWatch stopWatch) {
-            this.stopWatch = stopWatch;
+        public void setCalculator(final LightDeviationCalculator calculator) {
+            this.calculator = calculator;
         }
     }
 }
