4f42138c85e879b1216917fff27c6596a97cc606
Александр Викторов <alexandervas1985@outlook.com>
2019-12-07 15:36:13 +0300

    #Modify redis configuration

--- aggregator/src/main/java/com/soaesps/aggregator/config/RedisConfiguration.java
+++ aggregator/src/main/java/com/soaesps/aggregator/config/RedisConfiguration.java
@@ -1,38 +1,87 @@
 package com.soaesps.aggregator.config;
 
+import io.lettuce.core.ClientOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
-import java.util.Set;
+import java.util.Map;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfiguration {
     @Value("redis.master")
     private String master;
 
+    @Value("redis.port")
+    private Integer port;
+
+    @Value("redis.factory.type")
+    private String type;
+
     @Value("#{'${redis.address}'.split(',')}")
-    private Set<String> addresses;
+    private Map<String, Integer> addresses;
 
     @Bean
-    public RedisConnectionFactory jedisConnectionFactory() {
-        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
-                .master(master)
-                .sentinel("127.0.0.1", 26379)
-                .sentinel("127.0.0.1", 26380);
+    public RedisConnectionFactory redisConnectionFactory() {
+        switch (type) {
+            case "Jedis":
+                return jedisConnectionFactory();
+            case "Lettuce":
+                return lettuceConnectionFactory();
+            default:
+                return jedisConnectionFactory();
+        }
+    }
 
-        return new JedisConnectionFactory(sentinelConfig);
+    protected RedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory(sentinelConfiguration());
+    }
+
+    protected RedisConnectionFactory lettuceConnectionFactory() {
+        final RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(master, port);
+        final LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientOptions(clientOptions()).build();
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
+    }
+
+    protected RedisSentinelConfiguration sentinelConfiguration() {
+        final RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+                .master(master);
+        addresses.entrySet().stream().forEach(i -> {
+            sentinelConfig.sentinel(i.getKey(), i.getValue());
+        });
+
+        return sentinelConfig;
+    }
+
+    protected ClientOptions clientOptions() {
+        final ClientOptions options = ClientOptions.builder()
+                .autoReconnect(true).build();
+
+        return options;
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+    public RedisTemplate<String, Object> redisTemplate(final RedisConnectionFactory redisConnectionFactory) {
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
 
         return template;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(final RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.create(connectionFactory);
     }
 }
