2a990410041d02a283bd876c531f00b676ecd58e
Александр Викторов <alexandervas1985@outlook.com>
2019-12-09 01:40:42 +0300

    #Modify hibernate configuration

--- payments-service/src/main/java/com/soaesps/payments/config/HibernateConfiguration.java
+++ payments-service/src/main/java/com/soaesps/payments/config/HibernateConfiguration.java
@@ -6,9 +6,9 @@ import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -20,11 +20,13 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@EnableTransactionManagement
-@PropertySource("classpath:persistence-jndi.properties")
+@EntityScan({"com.soaesps.payments"})
 @EnableJpaRepositories(basePackages = {"com.soaesps.payments.repository"})
+@EnableTransactionManagement
 public class HibernateConfiguration {
     private static Logger log = LoggerFactory.getLogger(HibernateConfiguration.class);
+
+    private static final int DEFAULT_CONNECTION_POOL_SIZE = 4;
 
     @Autowired
     private Environment env;
@@ -41,14 +43,17 @@ public class HibernateConfiguration {
 
     @Bean
     public DataSource restDataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(env.getProperty("jdbc.url"));
-        config.setUsername(env.getProperty("jdbc.username"));
-        config.setPassword("jdbc.password");
-        config.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+        final HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(env.getProperty("spring.datasource.url"));
+        config.setUsername(env.getProperty("spring.datasource.username"));
+        config.setPassword(env.getProperty("spring.datasource.password"));
+        config.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        final String mps = env.getProperty("spring.datasource.hikari.maximum-pool-size");
+        config.setMaximumPoolSize(mps != null ? Integer.valueOf(mps) : DEFAULT_CONNECTION_POOL_SIZE);
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
         return new HikariDataSource(config);
     }
 
@@ -67,8 +72,8 @@ public class HibernateConfiguration {
 
     private Properties hibernateProperties() {
         Properties props = new Properties();
-        props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-        props.setProperty("hibernate.show_sql", "true");
+        props.setProperty("hibernate.dialect", env.getProperty("spring.datasource.jpa.database-platform"));
+        props.setProperty("hibernate.show_sql", env.getProperty("spring.datasource.jpa.show_sql"));
         props.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
         props.setProperty("hibernate.globally_quoted_identifiers", "true");
 
