0d4086e185f0d92c4ec98c0b71b6b3f9f20b7411
Александр Викторов <alexandervas1985@outlook.com>
2019-12-15 03:19:57 +0300

    #Modify bank account

--- payments-service/src/main/java/com/soaesps/payments/domain/transactions/BankAccount.java
+++ payments-service/src/main/java/com/soaesps/payments/domain/transactions/BankAccount.java
@@ -2,40 +2,27 @@ package com.soaesps.payments.domain.transactions;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.soaesps.core.DataModels.BaseEntity;
+import org.hibernate.annotations.Type;
 
 import javax.annotation.Nonnull;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "server_bills")//@Table(name = "soa_esps.server_bills") //name = "soa_esps.
+@Table(name = "server_bills")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class BankAccount extends BaseEntity {
-    /*@Id
-    @GenericGenerator(name="kaugen" , strategy="increment")
-    @GeneratedValue(generator="kaugen")
-    @Column(name = "id", nullable = false)
-    private Integer id;*/
-
     @Embedded
     private ServerBADesc serverBADesc;
 
     @Column(name = "indentation", length = 256, nullable = false)
     private String indentation;
 
-    //@Type(type="org.hibernate.type.BinaryType")
-    //@Column(name = "bill_signature", nullable = false)
-    @Transient
+    @Lob
+    @Type(type="org.hibernate.type.BinaryType")
+    @Column(name = "bill_signature", nullable = false)
     private byte[] billSignature;
 
     public BankAccount() {}
-
-    /*public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }*/
 
     @Nonnull
     public ServerBADesc getServerBADesc() {
