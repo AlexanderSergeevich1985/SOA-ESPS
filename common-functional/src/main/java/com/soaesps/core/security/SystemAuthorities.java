fe88ee1d5aed29663f3d9f4a2a40b92372023c33
Александр Викторов <alexandervas1985@outlook.com>
2020-03-30 23:24:19 +0300

    #Add SystemAuthorities

---
+++ common-functional/src/main/java/com/soaesps/core/security/SystemAuthorities.java
@@ -0,0 +1,15 @@
+package com.soaesps.core.security;
+
+public class SystemAuthorities {
+    public static final String ROLE_USER = "ROLE_USER";
+
+    public static final String ROLE_OWNER = "ROLE_OWNER";
+
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    public static final String ROLE_IS_ADMIN = "ROLE_IS_ADMIN";
+
+    public static final String ROLE_INNER_SERVICE = "ROLE_INNER_SERVICE";
+
+    public static final String ROLE_SUPPORTER = "ROLE_SUPPORTER";
+}
