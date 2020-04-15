5f3a0410d2ea63afa3b4e6eb170600f93d1b55b0
Александр Викторов <alexandervas1985@outlook.com>
2020-04-15 23:38:31 +0300

    #Add DeviceInfoRepository

---
+++ profile-service/src/main/java/com/soaesps/profile/repository/DeviceInfoRepository.java
@@ -0,0 +1,13 @@
+package com.soaesps.profile.repository;
+
+import com.soaesps.core.DataModels.device.DeviceInfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DeviceInfoRepository extends JpaRepository<DeviceInfo, Long> {
+    Optional<DeviceInfo> findByDeviceUUID(final String name);
+}
