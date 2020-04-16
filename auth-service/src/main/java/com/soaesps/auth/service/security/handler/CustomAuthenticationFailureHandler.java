4a99708dd05d224112407d570a5da286d67fcd45
Александр Викторов <alexandervas1985@outlook.com>
2020-04-16 14:18:06 +0300

    #Modify CustomAuthenticationFailureHandler

--- auth-service/src/main/java/com/soaesps/auth/service/security/handler/CustomAuthenticationFailureHandler.java
+++ auth-service/src/main/java/com/soaesps/auth/service/security/handler/CustomAuthenticationFailureHandler.java
@@ -8,14 +8,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+@Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    public final ErrorResponse errorResponse = new ErrorResponse();
+
     @Autowired
     private ObjectMapper mapper;
 
@@ -27,19 +32,32 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
         mapper.writeValue(response.getWriter(), errorResponse(this, ex));
     }
 
-    public class ErrorResponse {
-        private final AuthenticationException exception;
+    public static class ErrorResponse {
+        private AuthenticationException exception;
 
-        private final LocalDateTime timeStamp = LocalDateTime.now();
+        private LocalDateTime timeStamp;
+
+        public ErrorResponse() {}
 
         public ErrorResponse(final AuthenticationException ex) {
             this.exception = ex;
+        }
+
+        public ErrorResponse setException(final AuthenticationException exception) {
+            this.exception = exception;
+            this.timeStamp = LocalDateTime.now();
+
+            return this;
         }
     }
 
     static public ErrorResponse errorResponse(
             final CustomAuthenticationFailureHandler handler,
             final AuthenticationException ex) {
-        return handler.new ErrorResponse(ex);
+        try {
+            return handler.errorResponse.setException(ex);
+        } catch (final NullPointerException npe) {
+            return new ErrorResponse(ex);
+        }
     }
 }
