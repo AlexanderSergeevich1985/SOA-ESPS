97b1e087bd100c89012116ddfd8b15986fba56fe
Александр Викторов <alexandervas1985@outlook.com>
2020-04-18 02:58:20 +0300

    #Modify UserDetailsRepository

--- auth-service/src/main/java/com/soaesps/auth/repository/UserDetailsRepository.java
+++ auth-service/src/main/java/com/soaesps/auth/repository/UserDetailsRepository.java
@@ -1,12 +1,13 @@
 package com.soaesps.auth.repository;
 
 import com.soaesps.core.DataModels.security.BaseUserDetails;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserDetailsRepository extends CrudRepository<BaseUserDetails, Long> {
-    Optional<BaseUserDetails> findByName(String name);
+public interface UserDetailsRepository extends JpaRepository<BaseUserDetails, Long> {
+    Optional<BaseUserDetails> findByUserName(String name);
 }
--- auth-service/src/main/java/com/soaesps/auth/service/BaseUserDetailsServiceImpl.java
+++ auth-service/src/main/java/com/soaesps/auth/service/BaseUserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.soaesps.auth.service;
 
 import com.soaesps.auth.repository.UserDetailsRepository;
 import com.soaesps.core.DataModels.security.BaseUserDetails;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,42 +17,52 @@ public class BaseUserDetailsServiceImpl implements BaseUserDetailsService {
     private UserDetailsRepository repository;
 
     public UserDetails loadUserByUsername(final String userName) throws UsernameNotFoundException {
-        Optional<BaseUserDetails> result = this.repository.findByName(userName);
-        if(result == null) {
+        final Optional<BaseUserDetails> result = this.repository.findByUserName(userName);
+        if(result == null || !result.isPresent()) {
             throw new UsernameNotFoundException(userName);
         }
+
         return result.get();
     }
 
     public boolean createUserAccount(final BaseUserDetails userDetails) {
-        if(userDetails == null) return false;
-        Optional<BaseUserDetails> result = this.repository.findByName(userDetails.getUsername());
-        Assert.isTrue(!result.isPresent(), "profile already exists: " + userDetails.getUsername());
+        if(userDetails == null) {
+            return false;
+        }
+        final Optional<BaseUserDetails> result = this.repository.findByUserName(userDetails.getUsername());
+        Assert.isTrue(!result.isPresent(), "[BaseUserDetailsServiceImpl/createUserAccount]: profile already exists: " + userDetails.getUsername());
         this.repository.save(userDetails);
+
         return true;
     }
 
     public boolean updateUserAccount(final String name, final BaseUserDetails userDetails) {
-        if(name == null || name.isEmpty() || userDetails == null) return false;
-        Optional<BaseUserDetails> result = this.repository.findByName(name);
-        Assert.isTrue(result.isPresent(), "profile doesn't exists: " + userDetails.getUsername());
-        BaseUserDetails existing = result.get();
+        if(name == null || name.isEmpty() || userDetails == null) {
+            return false;
+        }
+        final Optional<BaseUserDetails> result = this.repository.findByUserName(name);
+        Assert.isTrue(result.isPresent(), "[BaseUserDetailsServiceImpl/updateUserAccount]: profile doesn't exists: " + userDetails.getUsername());
+        final BaseUserDetails existing = result.get();
 
         existing.setUserName(userDetails.getUsername());
         existing.setPassword(userDetails.getPassword());
         existing.setAuthorities(userDetails.getAuthorities());
 
         this.repository.save(existing);
+
         return true;
     }
 
     public boolean deleteUserAccount(final String name) {
-        if(name == null || name.isEmpty()) return false;
-        Optional<BaseUserDetails> result = this.repository.findByName(name);
-        Assert.isTrue(result.isPresent(), "profile doesn't exists: " + name);
-        BaseUserDetails existing = result.get();
+        if(name == null || name.isEmpty()) {
+            return false;
+        }
+        final Optional<BaseUserDetails> result = this.repository.findByUserName(name);
+        Assert.isTrue(result.isPresent(), "[BaseUserDetailsServiceImpl/deleteUserAccount]: profile doesn't exists: " + name);
+        final BaseUserDetails existing = result.get();
 
         this.repository.delete(existing);
+
         return true;
     }
 }
