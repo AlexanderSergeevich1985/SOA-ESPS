ddd1acae9097dc806ba7224433a516de221f2db7
Александр Викторов <alexandervas1985@outlook.com>
2022-04-14 02:49:38 +0300

    #Modify HttpUtils

--- common-functional/src/main/java/com/soaesps/core/Utils/HttpUtils.java
+++ common-functional/src/main/java/com/soaesps/core/Utils/HttpUtils.java
@@ -1,16 +1,11 @@
 package com.soaesps.core.Utils;
 
 import com.soaesps.core.patterns.Patterns;
-import org.jetbrains.annotations.NotNull;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.OK;
 
 public class HttpUtils {
     private HttpUtils() {
@@ -83,5 +78,87 @@ public class HttpUtils {
 
             return result;
         }
+    }
+
+    public enum ErrorMsg {
+        ON_USER_NOT_FOUND("ERRC01", ""),
+        ON_USER_ALREADY_EXIST("ERRC02", ""),
+        ON_ILLEGAL_ARGUMENT_EXCEPTION("ERRC03", ""),
+        ON_EXCEPTION("ERRC04", "");
+
+        private String errCode;
+
+        private String errMsg;
+
+        ErrorMsg(String errCode, String errMsg) {
+            this.errCode = errCode;
+            this.errMsg = errMsg;
+        }
+
+        public String getErrCode() {
+            return this.errCode;
+        }
+
+        public void setErrCode(String errCode) {
+            this.errCode = errCode;
+        }
+
+        public String getErrMsg() {
+            return errMsg;
+        }
+
+        public void setErrMsg(String errMsg) {
+            this.errMsg = errMsg;
+        }
+    }
+
+    public static <T> ResponseEntity<T> onOk(String... headers) {
+        return statusAndHeaders(HttpStatus.OK, headers).build();
+    }
+
+    public static <T> ResponseEntity<T> onOk(T value) {
+        ResponseEntity.BodyBuilder builder = statusAndHeaders(HttpStatus.OK);
+        builder.body(value);
+
+        return builder.build();
+    }
+
+    public static <T> ResponseEntity<T> onOk(T value, String... headers) {
+        ResponseEntity.BodyBuilder builder = statusAndHeaders(HttpStatus.OK, headers);
+        builder.body(value);
+
+        return builder.build();
+    }
+
+    public static <T> ResponseEntity<T> onError(ErrorMsg error, String... headers) {
+        ResponseEntity.BodyBuilder builder = statusAndHeaders(HttpStatus.INTERNAL_SERVER_ERROR, headers);
+        builder.header("x-error-code", error.getErrCode());
+        builder.header("X-Error-Info", error.getErrMsg());
+
+        return builder.build();
+    }
+
+    public static <T> ResponseEntity<T> onError(Exception ex, String... headers) {
+        ResponseEntity.BodyBuilder builder = statusAndHeaders(HttpStatus.INTERNAL_SERVER_ERROR, headers);
+        builder.header("x-error-code", ex.getClass().getName());
+        builder.body(ex.getLocalizedMessage());
+
+        return builder.build();
+    }
+
+    public static <T> ResponseEntity<T> headersAndBody(HttpStatus status, T value, String... headers) {
+        ResponseEntity.BodyBuilder builder = statusAndHeaders(status, headers);
+        builder.body(value);
+
+        return builder.build();
+    }
+
+    public static ResponseEntity.BodyBuilder statusAndHeaders(HttpStatus status, String... headers) {
+        ResponseEntity.BodyBuilder builder = ResponseEntity.status(status);
+        for (int i = 0; i < headers.length - 1; i += 2) {
+            builder.header(headers[i], headers[i+1]);
+        }
+
+        return builder;
     }
 }
