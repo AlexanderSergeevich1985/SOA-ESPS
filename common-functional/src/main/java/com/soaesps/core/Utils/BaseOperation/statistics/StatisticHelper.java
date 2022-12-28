2e7eaed4b25ec37ce2903149232de9d615b61c5f
AlexanderSergeevich1985 <zbrauberg@mail.ru>
2022-12-29 00:18:12 +0300

    #Modify StatisticHelper

--- common-functional/src/main/java/com/soaesps/core/Utils/BaseOperation/statistics/StatisticHelper.java
+++ common-functional/src/main/java/com/soaesps/core/Utils/BaseOperation/statistics/StatisticHelper.java
@@ -10,4 +10,14 @@ public class StatisticHelper {
     static public int getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
     }
+
+    static public int getRandomInt(int numOfBits) {
+        StringBuilder sb = new StringBuilder();
+        int maxOfBits = numOfBits < 32 ? numOfBits : 31;
+        for (int i = 0; i < maxOfBits; ++i) {
+            sb.append(itIsHappened(0.5) ? 1 : 0);
+        }
+
+        return Integer.parseInt(sb.toString(), 2);
+    }
 }
